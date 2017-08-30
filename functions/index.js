891176a49cb86cdcecbf10c15106f23c0dc55df5
Israel Blancas <iblancasa@gmail.com>
2017-08-31 01:04:40 +0200

    Remove Javascript warnings

--- functions/index.js
+++ functions/index.js
@@ -1,10 +1,11 @@
 const functions = require('firebase-functions');
 
 exports.updateVotes = functions.database.ref('/meme-votes/{memeId}')
-  .onWrite(event => {
+  .onWrite((event) => {
     const votes = event.data.val();
     let sum = Object.keys(votes)
-      .map(function(k){return votes[k]})
+      .map(function(k) {return votes[k];})
       .reduce((a, b) => a + b, 0);
-    return event.data.ref.parent.parent.child(`/memes/${event.params.memeId}/votes`).set(sum);
+    var node = event.data.ref.parent.parent;
+    return node.child(`/memes/${event.params.memeId}/votes`).set(sum);
   });
